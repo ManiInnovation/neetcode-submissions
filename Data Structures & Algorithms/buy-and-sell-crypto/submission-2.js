0bca4cd109b8fc561b285cd9e27b79c66e5a352b
ManiInnovation <manitradecraft@gmail.com>
2026-03-31 20:43:46 +0530

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(arr) {
+        let i = 0;
+        let j = i+1;
+        let maxProfit = 0;
+        while(i<j & j<arr.length){
+            if(arr[i]>= arr[j]){
+                if(maxProfit !== 0){
+                    i = j;
+                    j++
+                }else{
+                i++
+                j++
+                }
+            }else if(arr[i]< arr[j]){
+                maxProfit = Math.max(maxProfit,arr[j]-arr[i])
+                j++
+            }
+        }
+        return maxProfit
+    }
+}
