8023538cb1658644005b9ef379831d08d6647142
ManiInnovation <manitradecraft@gmail.com>
2026-04-06 18:38:19 +0530

    Add: reverse-nodes-in-k-group - submission-4

---
+++ Data Structures & Algorithms/reverse-nodes-in-k-group/submission-4.js
@@ -0,0 +1,55 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {ListNode} head
+     * @param {number} k
+     * @return {ListNode}
+     */
+    reverseKGroup(head, k) {
+        let curr = head;
+        let i = 0;
+        let dummyHead = new ListNode(0)
+        let origin = head;
+        let lastNode = null;
+        while(curr != null){
+            console.log(curr?.val, i)
+            if(i === k-1){
+                let tmp = curr.next;
+                curr.next = null;
+                let prev = null;
+                let c = origin;
+                while(c !== null){
+                    let tmp = c.next;
+                    c.next = prev;
+                    prev = c;
+                    c = tmp;
+                }
+
+                if(lastNode == null){
+                dummyHead.next = prev;
+                }else{
+                lastNode.next = prev;
+                }
+
+                lastNode = origin;
+                lastNode.next = tmp;
+                i=0
+                curr = tmp;
+                origin = tmp;
+            }else{
+            curr = curr.next;
+            i++
+            }
+        }
+        return dummyHead.next;
+    }
+}
